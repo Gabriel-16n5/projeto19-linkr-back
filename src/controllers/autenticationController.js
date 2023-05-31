860576ea9039d21136f0093866fe479fdd2f0f1d
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-31 15:32:32 -0300

    minor fix signup DONE

--- src/controllers/autenticationController.js
+++ src/controllers/autenticationController.js
@@ -31,19 +31,18 @@ export async function loginAccount(req, res) {
             // Verificar se o e-mail está cadastrado
             const usuario = await db.query('SELECT * FROM users WHERE email = $1', [email])
             if (usuario.rowCount === 0) return res.status(404).send("usuário não encontrado")
-    
             // Verificar se a senha digitada corresponde com a criptografada
-            const senhaEstaCorreta = bcrypt.compareSync(password, usuario.senha)
+            const senhaEstaCorreta = bcrypt.compareSync(password, usuario.rows[0].password)
             if (!senhaEstaCorreta) return res.status(401).send("Senha incorreta")
     
             // Se deu tudo certo, criar um token para enviar ao usuário
             const token = uuid()
     
             // Guardaremos o token e o id do usuário para saber que ele está logado
-            await db.query(`INSERT INTO sessoes (token, "userId") VALUES ($1, $2)`, [token, usuario.rows[0].id])
+            await db.query(`INSERT INTO sessions (token, "idUser") VALUES ($1, $2)`, [token, usuario.rows[0].id])
     
             // Finalizar com status de sucesso e enviar token para o cliente
-            res.status(201).send(token)
+            res.status(201).send({token})
     
         } catch (err) {
         res.send(err.message)
