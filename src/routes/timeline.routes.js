2c5e6622e8f7a24fb7727ccfeab891e9c58f3a4d
curtyraissa <raissacurty@gmail.com>
2023-05-31 01:50:08 -0300

    criacao rotas de timeline

---
+++ src/routes/timeline.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { createPost, getPost, deletePost, editPost } from "../controllers/timeline.controller.js"
+import { auth } from "../middlewares/auth.middlewares.js"
+import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+import {postsSchema} from "../schemas/posts.schemas.js";
+
+const timelineRouter = Router()
+
+timelineRouter.post("/timeline", auth, validateSchema(postsSchema), createPost);
+timelineRouter.get("/timeline", auth, getPost);
+timelineRouter.delete("/timeline:id", auth, deletePost);
+timelineRouter.put("/timeline:id", auth, editPost);
+
+export default timelineRouter;
