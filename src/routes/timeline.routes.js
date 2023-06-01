b9dd5984cc30fca0317b196f77b1a14e86279358
Victor Mafra <victorvmafra97@gmail.com>
2023-06-01 12:47:16 -0300

    added post and delete likes

--- src/routes/timeline.routes.js
+++ src/routes/timeline.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPost, getPost, deletePost, editPost } from "../controllers/timeline.controller.js"
+import { createPost, getPost, deletePost, editPost, postLikes, deleteLikes } from "../controllers/timeline.controller.js"
 // import { auth } from "../middlewares/auth.middlewares.js"
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import {postSchema} from "../schemas/posts.schemas.js";
@@ -8,6 +8,8 @@ const timelineRouter = Router()
 
 timelineRouter.post("/timeline", validateSchema(postSchema), createPost);
 timelineRouter.get("/timeline", getPost);
+timelineRouter.post("/likes", postLikes);
+timelineRouter.delete("/likes", deleteLikes)
 timelineRouter.delete("/timeline/:id", deletePost);
 timelineRouter.put("/timeline/:id", editPost);
 
