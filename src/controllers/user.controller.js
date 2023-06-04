b58afd44f66b48188304f92a3f3889d735d041a8
GustavoOlimpio <gustavo-olimpio@hotmail.com>
2023-06-04 17:11:23 -0300

    feat/userById

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,52 @@
 import { db } from "../database/database.connection.js";
-
+import urlMetadata from "url-metadata";
+import fetch from "node-fetch";
+global.fetch = fetch;
 export async function getPostsUser(req, res) {
     const {id} = req.params
     try{
+        const posts = await db.query(
+            `SELECT posts.*,users.username,users."pictureUrl"
+            FROM posts 
+            JOIN sessions ON sessions.id=posts."idSession" 
+            JOIN users ON sessions."idUser"=users.id 
+            WHERE users.id=${id}
+            ORDER BY posts.id DESC LIMIT 20;`
+          );
+          const allPosts = posts.rows
+    
+          let array = []
 
-        res.send("oi");
+          for (let i=0;i<posts.rowCount;i++){  
+            let likes = await db.query(`SELECT users.username FROM likes JOIN users ON likes."userId"=users.id WHERE likes."postId"=${allPosts[i].id};`)  
+            await urlMetadata(allPosts[i].url)
+              .then((metadata) => {
+                if (likes.rows===undefined){
+                  likes.rows=[]
+                }
+                const object = {
+                  postId:allPosts[i].id,
+                  username:allPosts[i].username,
+                  text:allPosts[i].text,
+                  pictureUrl:allPosts[i].pictureUrl,
+                  title:metadata['og:title'],
+                  description:metadata['og:description'],
+                  url:metadata.url,
+                  image:metadata['og:image'],
+                  peopleLike:likes.rows
+                }
+                  array.push(object) 
+              },
+                (err) => {
+                  console.log(err)
+                })
+                
+            }
+
+
+        res.status(201).send(array);
     } catch (erro){
-        res.send(erro.message)
+        res.status(500).send(erro.message)
     }
 }
 
