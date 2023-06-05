ce77d75f1f6376f51c42f9a06d3ef4a9e3a06431
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-06-05 17:41:17 -0300

    parcial function feat

--- src/routes/hashtag.routes.js
+++ src/routes/hashtag.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 // import { createPost, getPost, deletePost, editPost, postLikes, deleteLikes } from "../controllers/timeline.controller.js"
-import {getTrending,getPostsByHashtag} from "../controllers/hashtag.controller.js";
+import {getTrending,getPostsByHashtag, postTags} from "../controllers/hashtag.controller.js";
 const hashtagRouter = Router()
 
+hashtagRouter.post("/hashtag", postTags);
 hashtagRouter.get("/hashtag", getTrending);
 hashtagRouter.get("/hashtag/:hashtag", getPostsByHashtag);
 
